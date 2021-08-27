5b08cd5f66e26628f4346b16a3987abd8c91fb1a
ReevesAk <reeves.akwa@gmail.com>
2021-08-27 09:45:50 +0100

    check error in api tags, create server side cache(sqlite), remove repeated posts, and comment on code.

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 import time
 import flask
 import requests
+import sys
 import json
 import threading
+import requests_cache
+from nose.tools import assert_true
 
-apiResponseForTech = "https://api.hatchways.io/assessment/blog/posts?tag=tech"
-apiResponseForHealth = "https://api.hatchways.io/assessment/blog/posts?tag=health"
-apiResponseForStartups = "https://api.hatchways.io/assessment/blog/posts?tag=startups"
-apiResponseForHistory = "https://api.hatchways.io/assessment/blog/posts?tag=history"
-apiResponseForScience = "https://api.hatchways.io/assessment/blog/posts?tag=science"
-apiResponseForCulture = "https://api.hatchways.io/assessment/blog/posts?tag=culture"
-apiResponseForPolitics = "https://api.hatchways.io/assessment/blog/posts?tag=politics"
-apiResponseForDesign = "https://api.hatchways.io/assessment/blog/posts?tag=design  "    
+# Server side cache to API requests.
+# This is used to avoid making too many API requests calls.
+requests_cache.install_cache("hatchwayCache", backend='SQLite', expire_after=-1)
 
+techTag = {"tag" : "tech"}
+historyTag = {"tag" : "history"}
+designTag = {"tag" : "design"}
+cultureTag = {"tag" : "culture"}
+startupsTag = {"tag" : "startups"}
+scienceTag = {"tag" : "science"}
+politicsTag = {"tag" : "politics"}
+healthTag = {"tag" : "health"}
 
-apiCallList = [apiResponseForCulture, apiResponseForDesign, apiResponseForHealth, apiResponseForHistory, 
-    apiResponseForPolitics, apiResponseForStartups, apiResponseForScience, apiResponseForTech
-]
-
+# API url.
+api = "https://api.hatchways.io/assessment/blog/posts"
 
 localThread = threading.local()
 
-
-def printJson(obj):
-    # Create a formatted string of the Python JSON object.
-    text = json.loads(obj, sort_keys=True, indent=4)
-    print(text)
-
-
+# Initialize flask for API server.
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -38,70 +36,118 @@ def getSession():
     return localThread.session
 
 
-def singleFetch(api_url):
+def singleFetch(api_url, queryTag):
     session = getSession()
-    with session.get(api_url) as response:
+    with session.get(api_url, params=queryTag) as response:
         return response.text
 
 
-
+# API ping route. 
 @app.route('/api/ping', methods=['GET'])
 def pingApi():
     resp = flask.jsonify(success=True)
     return resp
    
 
-
+# Fetches result for API url with tech tag.
 def fetchTechTag():
-    fetchResult = singleFetch(apiResponseForTech)
-    techLoad = json.loads(fetchResult)
-    return techLoad
+    try:
+        fetchResult = singleFetch(api, techTag)
+        techLoad = json.loads(fetchResult)
+        return techLoad
+        # Check and catch missing tag error.
+    except:
+        fetchResult = singleFetch(api, "")
+        return flask.jsonify(error="Tags parameter is required")
 
 
+# Fetches result for API url with design tag.
 def fetchDesignTag():
-    fetchResult = singleFetch(apiResponseForDesign)
-    designLoad = json.loads(fetchResult)
-    return designLoad
+    try:
+        fetchResult = singleFetch(api, designTag)
+        designLoad = json.loads(fetchResult)
+        return designLoad
+        # Check and catch missing tag error.
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
 
 
+# Fetches result for API url with culture tag.
 def fetchCultureTag():
-    fetchResult = singleFetch(apiResponseForCulture)
-    cultureLoad = json.loads(fetchResult)
-    return cultureLoad
+    try:
+        fetchResult = singleFetch(api, cultureTag)
+        cultureLoad = json.loads(fetchResult)
+        return cultureLoad
+        # Check and catch missing tag error.
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
+    
 
 
-
+# Fetches result for API url with science tag.
 def fetchScienceTag():
-    fetchResult = singleFetch(apiResponseForScience)
-    scienceLoad = json.loads(fetchResult)
-    return scienceLoad
+    try:
+        fetchResult = singleFetch(api, scienceTag)
+        scienceLoad = json.loads(fetchResult)
+        return scienceLoad
+        # Check and catch missing tag error.
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
 
 
 
+# Fetches result for API url with startups tag.
 def fetchStartupsTag():
-    fetchResult = singleFetch(apiResponseForStartups)
-    startupLoad = json.loads(fetchResult)
-    return startupLoad
+    try:   
+        fetchResult = singleFetch(api, startupsTag)
+        startupLoad = json.loads(fetchResult)
+        return startupLoad
+        # Check and catch missing tag error. 
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
+    
 
-
+# Fetches result for API url with health tag.
 def fetchHealthTag():
-    fetchResult = singleFetch(apiResponseForHealth)
-    startupLoad = json.loads(fetchResult)
-    return startupLoad
+    try:
+        fetchResult = singleFetch(api, healthTag)
+        startupLoad = json.loads(fetchResult)
+        return startupLoad
+        # Check and catch missing tag error. 
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
+    
 
-
+# Fetches result for API url with history tag.
 def fetchHistoryTag():
-    fetchResult = singleFetch(apiResponseForHistory)
-    historyLoad = json.loads(fetchResult)
-    return historyLoad
+    try:
+        fetchResult = singleFetch(api, historyTag)
+        historyLoad = json.loads(fetchResult)
+        return historyLoad
+        # Check and catch missing tag error. 
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
+    
 
-
+# Fetches result for API url with politics tag.
 def fetchPoliticsTag():
-   fetchResult = singleFetch(apiResponseForPolitics)
-   politicsLoad = json.loads(fetchResult)
-   return politicsLoad
+    try:
+        fetchResult = singleFetch(api, politicsTag)
+        politicsLoad = json.loads(fetchResult)
+        return politicsLoad
+        # Check and catch missing tag error. 
+    except:
+        fetchResult = singleFetch(api, "")    
+        return flask.jsonify(error="Tags parameter is required")
+    
 
-
+# Combines all the results from the API requests.
 def collateAllTagPosts(*listOfAllPost):
     return listOfAllPost
 
@@ -115,14 +161,32 @@ politics = fetchPoliticsTag
 science = fetchScienceTag
 health = fetchHealthTag
 
+
+# API posts route.
 @app.route('/api/posts', methods=['GET'])
 def getll():
-    collation = collateAllTagPosts(tech(),startup(),design(),culture(), history(), politics(),science(), health())
-    values = ','.join([str(i) for i in collation])
-
-    return values
+    collationOfTags = collateAllTagPosts(tech(), culture(), startup(), design(), history(),
+                politics(), science(), health())
 
 
-if __name__ == '__main__':   
+    # Removes repeated posts from the collated API responses.
+    filteredPosts=set()
+    result=[element for element in collationOfTags
+    if not (tuple(element) in filteredPosts
+        or  filteredPosts.add(tuple(element)))]
+    value = json.dumps(result, indent=4)
+    return str(value)
+
+
+# Test for each API tag by passing in the API url and it's accompanying tag
+def test_request_response():
+    # Send a request to the API server and store the response.
+    response = requests.get(api, params=techTag)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_true(response.ok)
+
+if __name__ == '__main__':      
     startTime = time.time()
+    # Runs the app.
     app.run()
